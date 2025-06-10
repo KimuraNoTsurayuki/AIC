192d7bd1d3c11c3b25c7859bea647cd6984155f4
KimuraNoTsurayuki <aleksandre@tbilisi-real-estate.net>
2025-06-10 15:04:49 +0400

    Added Some Colour

--- AIC.py
+++ AIC.py
@@ -2,6 +2,7 @@ from reportlab.pdfgen import canvas
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.lib import colors
+from reportlab.lib.colors import Color, red, blue, green, black 
 from PIL import Image
 
 #Coordinate (0,0) is bottom left. Every coordinate points to the bottom left.
@@ -56,11 +57,15 @@ def createTable(n,descs,quants,tots,currency,tax):
 	c.line(table_coords[0]+x_offset_2*2,table_coords[1]-(n+3)*y_offset-y_offset_3,table_coords[0]+x_offset_2*3,table_coords[1]-(n+3)*y_offset-y_offset_3)
 	c.drawString(table_coords[0]+x_offset_1+x_offset_2*2,table_coords[1]-y_offset_3*2-y_offset*(n+7/2),str(a*(1+tax/100)))	
 	c.line(table_coords[0]+x_offset_2*2,table_coords[1]-(n+4)*y_offset-y_offset_3,table_coords[0]+x_offset_2*3,table_coords[1]-(n+4)*y_offset-y_offset_3)
-	c.line(table_coords[0],table_coords[1]-(n+4)*y_offset-y_offset_3,table_coords[0]+x_offset_2,table_coords[1]-(n+4)*y_offset-y_offset_3)	
+	#c.line(table_coords[0],table_coords[1]-(n+4)*y_offset-y_offset_3,table_coords[0]+x_offset_2,table_coords[1]-(n+4)*y_offset-y_offset_3)	
 	#---------------------------------------
-	c.drawString(table_coords[0]+x_offset_1,table_coords[1]-y_offset_3-y_offset_3-y_offset*(n+3/2),"Sum Total")
-	c.drawString(table_coords[0]+x_offset_1,table_coords[1]-y_offset_3-y_offset_3-y_offset*(n+5/2),"Tax (%)")
-	c.drawString(table_coords[0]+x_offset_1,table_coords[1]-y_offset_3-y_offset_3-y_offset*(n+7/2),"Sum Total with tax")	
+	c.setFillColor(blue, alpha=0.05)
+	c.rect(table_coords[0]-x_offset_1*2,table_coords[1]-y_offset_3*2-y_offset*(n+18/2)-y_offset_2*11-y_offset_3,x_offset_2*3+x_offset_1*4,y_offset_3*5+y_offset*(n+18/2)+y_offset_2*11,fill=True,stroke=False)
+	c.setFillColor(black)
+	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset_3-y_offset*(n+3/2),"Sum Total")
+	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset_3-y_offset*(n+5/2),"Tax (%)")
+	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset_3-y_offset*(n+7/2),"Sum Total with tax")	
+	c.setFont("Helvetica",9)
 	#--------------------------------------------------------------------------------
 	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+15/2)-y_offset_2*4,"Payment Instructions (TBC):")
 	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+15/2)-y_offset_2*5,"JSC TBC Bank")
@@ -73,10 +78,10 @@ def createTable(n,descs,quants,tots,currency,tax):
 	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+12/2)-y_offset_2*3,"GE93BG0000000607816171")
 	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+12/2)-y_offset_2*4,"Prime Management LLC")
 	#--------------------------------------------------------------------------------
-	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset*(n+15/2)-y_offset_2*5,"Company address: Georgia, Tbilisi,")
-	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset*(n+15/2)-y_offset_2*6,"Krtsanisi district, Rustavi highway,")
-	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset*(n+15/2)-y_offset_2*7,"N19, building N3, floor N1, storeroom")
-	c.drawString(table_coords[0],table_coords[1]-y_offset_3-y_offset*(n+15/2)-y_offset_2*8,"Company postal code: 0114")
+	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+18/2)-y_offset_2*8,"Company address: Georgia, Tbilisi,")
+	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+18/2)-y_offset_2*9,"Krtsanisi district, Rustavi highway,")
+	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+18/2)-y_offset_2*10,"N19, building N3, floor N1, storeroom")
+	c.drawString(table_coords[0]+x_offset_2*2,table_coords[1]-y_offset_3-y_offset*(n+18/2)-y_offset_2*11,"Company postal code: 0114")
 	createSkeleton(table_coords[0],table_coords[1]-y_offset_3-y_offset*(n+12/2))
 	
 def askForData():
@@ -109,10 +114,12 @@ def askForData():
 			full_name = "Mr. " + surname + " " + name
 		case "2":
 			full_name = "Ms. " + surname + " " + name
+	c.setFont("Helvetica",9)
 	c.drawString(table_coords[0]+80,table_coords[1]-y_offset_3-y_offset*(number_of_descriptions+12/2),full_name)
 	c.drawString(table_coords[0]+80,table_coords[1]-y_offset_3-y_offset*(number_of_descriptions+12/2)-y_offset_2,date)
 	c.drawString(table_coords[0]+80,table_coords[1]-y_offset_3-y_offset*(number_of_descriptions+12/2)-y_offset_2*2,createInvoiceID(deslashify(date),createSurnameShortform(surname),invoice_number))
 	c.drawString(table_coords[0]+80,table_coords[1]-y_offset_3-y_offset*(number_of_descriptions+12/2)-y_offset_2*3,due_date_sel)
+	c.setFont("Helvetica",12)
 	createTable(number_of_descriptions,descriptions,quantities,totals,currency_selection,tax_rate)											
 	
 def createSurnameShortform(surn):
